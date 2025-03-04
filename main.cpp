7d87e0fab25a993ea11b154b49615c3a7998dfb6
simrank <132793467+simrank13@users.noreply.github.com>
2025-03-04 13:30:46 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,61 @@
+#include <QApplication>
+#include <QMainWindow>
+#include <QTabWidget>
+#include <QVBoxLayout>
+#include <QLabel>
+#include <QWidget>
+
+
+class MainWindow : public QMainWindow {
+public:
+    MainWindow(QWidget *parent = nullptr) : QMainWindow(parent) {
+        setWindowTitle("Business Management System");
+        resize(800, 600);
+
+        QWidget *centralWidget = new QWidget(this);
+        setCentralWidget(centralWidget);
+
+        QTabWidget *tabWidget = new QTabWidget(centralWidget);
+
+        QWidget *dashboardPage = new QWidget();
+        QWidget *financialsPage = new QWidget();
+        QWidget *inventoryPage = new QWidget();
+        QWidget *budgetPage = new QWidget();
+
+        tabWidget->addTab(dashboardPage, "Dashboard");
+        tabWidget->addTab(financialsPage, "Financials");
+        tabWidget->addTab(inventoryPage, "Inventory");
+        tabWidget->addTab(budgetPage, "Budget Page");
+
+        QVBoxLayout *layout = new QVBoxLayout(centralWidget);
+        layout->addWidget(tabWidget);
+        centralWidget->setLayout(layout);
+
+        QLabel *dashboardLabel = new QLabel("", dashboardPage);
+        QVBoxLayout *dashboardLayout = new QVBoxLayout(dashboardPage);
+        dashboardLayout->addWidget(dashboardLabel);
+        dashboardPage->setLayout(dashboardLayout);
+
+        QLabel *financialsLabel = new QLabel("", financialsPage);
+        QVBoxLayout *financialsLayout = new QVBoxLayout(financialsPage);
+        financialsLayout->addWidget(financialsLabel);
+        financialsPage->setLayout(financialsLayout);
+
+        QLabel *inventoryLabel = new QLabel("", inventoryPage);
+        QVBoxLayout *inventoryLayout = new QVBoxLayout(inventoryPage);
+        inventoryLayout->addWidget(inventoryLabel);
+        inventoryPage->setLayout(inventoryLayout);
+
+        QLabel *budgetLabel = new QLabel("", budgetPage);
+        QVBoxLayout *budgetLayout = new QVBoxLayout(budgetPage);
+        budgetLayout->addWidget(budgetLabel);
+        budgetPage->setLayout(budgetLayout);
+    }
+};
+
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+    MainWindow window;
+    window.show();
+    return app.exec();
+}
